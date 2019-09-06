a5787c7898a22cdcf2a635c2babd3167b646eb29
Bojan <bojan@softwarehaus.io>
2019-09-06 17:59:41 +0200

    Rename View

---
+++ src/views/admin/AdminPanel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <section class="section">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+import { db } from '../../main'
+
+export default {
+  name: 'admin',
+  
+}
+</script>
